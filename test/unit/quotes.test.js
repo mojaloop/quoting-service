bf0e88d98cbb92d31d8c1dba0f8fe456d4da9705
Juan Correa <juan.correa@modusbox.com>
2019-10-18 03:16:23 -0700

    Issue1016-UnitTestCoverageAddRemainingResourcesToTheExistingPostQuotes

--- test/unit/quotes.test.js
+++ test/unit/quotes.test.js
@@ -44,6 +44,59 @@ const helper = require('../util/helper')
  */
 describe('/quotes', function () {
   /**
+   * summary: Quotes
+   * description: The HTTP request PUT /quotes/{ID}/error is used to request the creation of a quote for the provided financial transaction in the server.
+   * parameters: body, Accept, Content-Length, Content-Type, Date, X-Forwarded-For, FSPIOP-Source, FSPIOP-Destination, FSPIOP-Encryption, FSPIOP-Signature, FSPIOP-URI, FSPIOP-HTTP-Method
+   * produces: application/json
+   * responses: 202, 400, 401, 403, 404, 405, 406, 501, 503
+   */
+  test('test Bulk Error by ID put operation', async () => {
+    const server = new Hapi.Server()
+
+    await server.register([{
+      plugin: HapiOpenAPI,
+      options: {
+        api: Path.resolve(__dirname, '../../src/interface/swagger.json'),
+        handlers: Path.join(__dirname, '../../src/handlers'),
+        outputvalidation: true
+      }
+    }, ErrorHandler])
+
+    const requests = new Promise((resolve, reject) => {
+      Mockgen().requests({
+        path: '/quotes/{ID}/error',
+        operation: 'put'
+      }, function (error, mock) {
+        return error ? reject(error) : resolve(mock)
+      })
+    })
+
+    const mock = await requests
+
+    expect(mock).toBeTruthy()
+    expect(mock.request).toBeTruthy()
+    // Get the resolved path from mock request
+    // Mock request Path templates({}) are resolved using path parameters
+    const options = {
+      method: 'put',
+      url: mock.request.path,
+      headers: helper.defaultHeaders()
+    }
+    if (mock.request.body) {
+      // Send the request body
+      options.payload = mock.request.body
+    } else if (mock.request.formData) {
+      // Send the request form data
+      options.payload = mock.request.formData
+      // Set the Content-Type as application/x-www-form-urlencoded
+      options.headers = helper.defaultHeaders()
+    }
+
+    const response = await server.inject(options)
+
+    expect(response.statusCode).toBe(200)
+  })
+  /**
      * summary: Quotes
      * description: The HTTP request POST /quotes is used to request the creation of a quote for the provided financial transaction in the server.
      * parameters: body, Accept, Content-Length, Content-Type, Date, X-Forwarded-For, FSPIOP-Source, FSPIOP-Destination, FSPIOP-Encryption, FSPIOP-Signature, FSPIOP-URI, FSPIOP-HTTP-Method
@@ -95,5 +148,349 @@ describe('/quotes', function () {
     const response = await server.inject(options)
 
     expect(response.statusCode).toBe(202)
+  })
+  /**
+   * summary: Quotes
+   * description: The HTTP request GET /quotes/{ID} is used to request the retrieval of a quote for the provided financial transaction in the server.
+   * parameters: Accept, Content-Length, Content-Type, Date, X-Forwarded-For, FSPIOP-Source, FSPIOP-Destination, FSPIOP-Encryption, FSPIOP-Signature, FSPIOP-URI, FSPIOP-HTTP-Method
+   * produces: application/json
+   * responses: 202, 400, 401, 403, 404, 405, 406, 501, 503
+   */
+  test('test Quotes by ID get operation', async () => {
+    const server = new Hapi.Server()
+
+    await server.register([{
+      plugin: HapiOpenAPI,
+      options: {
+        api: Path.resolve(__dirname, '../../src/interface/swagger.json'),
+        handlers: Path.join(__dirname, '../../src/handlers'),
+        outputvalidation: true
+      }
+    }, ErrorHandler])
+
+    const requests = new Promise((resolve, reject) => {
+      Mockgen().requests({
+        path: '/quotes/{ID}',
+        operation: 'get'
+      }, function (error, mock) {
+        return error ? reject(error) : resolve(mock)
+      })
+    })
+
+    const mock = await requests
+
+    expect(mock).toBeTruthy()
+    expect(mock.request).toBeTruthy()
+    // Get the resolved path from mock request
+    // Mock request Path templates({}) are resolved using path parameters
+    const options = {
+      method: 'get',
+      url: mock.request.path,
+      headers: helper.defaultHeaders()
+    }
+
+    const response = await server.inject(options)
+
+    expect(response.statusCode).toBe(202)
+  })
+  /**
+   * summary: Quotes
+   * description: The HTTP request PUT /quotes/{ID} is used to request the creation of a quote for the provided financial transaction in the server.
+   * parameters: body, Accept, Content-Length, Content-Type, Date, X-Forwarded-For, FSPIOP-Source, FSPIOP-Destination, FSPIOP-Encryption, FSPIOP-Signature, FSPIOP-URI, FSPIOP-HTTP-Method
+   * produces: application/json
+   * responses: 202, 400, 401, 403, 404, 405, 406, 501, 503
+   */
+  test('test Quotes by ID put operation', async () => {
+    const server = new Hapi.Server()
+
+    await server.register([{
+      plugin: HapiOpenAPI,
+      options: {
+        api: Path.resolve(__dirname, '../../src/interface/swagger.json'),
+        handlers: Path.join(__dirname, '../../src/handlers'),
+        outputvalidation: true
+      }
+    }, ErrorHandler])
+
+    const requests = new Promise((resolve, reject) => {
+      Mockgen().requests({
+        path: '/quotes/{ID}',
+        operation: 'put'
+      }, function (error, mock) {
+        return error ? reject(error) : resolve(mock)
+      })
+    })
+
+    const mock = await requests
+
+    expect(mock).toBeTruthy()
+    expect(mock.request).toBeTruthy()
+    // Get the resolved path from mock request
+    // Mock request Path templates({}) are resolved using path parameters
+    const options = {
+      method: 'put',
+      url: mock.request.path,
+      headers: helper.defaultHeaders()
+    }
+    if (mock.request.body) {
+      // Send the request body
+      options.payload = mock.request.body
+    } else if (mock.request.formData) {
+      // Send the request form data
+      options.payload = mock.request.formData
+      // Set the Content-Type as application/x-www-form-urlencoded
+      options.headers = helper.defaultHeaders()
+    }
+
+    const response = await server.inject(options)
+
+    expect(response.statusCode).toBe(202)
+  })
+  /**
+   * summary: Quotes
+   * description: The HTTP request PUT /bulkQuotes/{ID}/error is used to request the creation of a quote for the provided financial transaction in the server.
+   * parameters: body, Accept, Content-Length, Content-Type, Date, X-Forwarded-For, FSPIOP-Source, FSPIOP-Destination, FSPIOP-Encryption, FSPIOP-Signature, FSPIOP-URI, FSPIOP-HTTP-Method
+   * produces: application/json
+   * responses: 202, 400, 401, 403, 404, 405, 406, 501, 503
+   */
+  test('test Bulk Quotes Error by ID put operation', async () => {
+    const server = new Hapi.Server()
+
+    await server.register([{
+      plugin: HapiOpenAPI,
+      options: {
+        api: Path.resolve(__dirname, '../../src/interface/swagger.json'),
+        handlers: Path.join(__dirname, '../../src/handlers'),
+        outputvalidation: true
+      }
+    }, ErrorHandler])
+
+    const requests = new Promise((resolve, reject) => {
+      Mockgen().requests({
+        path: '/bulkQuotes/{ID}/error',
+        operation: 'put'
+      }, function (error, mock) {
+        return error ? reject(error) : resolve(mock)
+      })
+    })
+
+    const mock = await requests
+
+    expect(mock).toBeTruthy()
+    expect(mock.request).toBeTruthy()
+    // Get the resolved path from mock request
+    // Mock request Path templates({}) are resolved using path parameters
+    const options = {
+      method: 'put',
+      url: mock.request.path,
+      headers: helper.defaultHeaders()
+    }
+    if (mock.request.body) {
+      // Send the request body
+      options.payload = mock.request.body
+    } else if (mock.request.formData) {
+      // Send the request form data
+      options.payload = mock.request.formData
+      // Set the Content-Type as application/x-www-form-urlencoded
+      options.headers = helper.defaultHeaders()
+    }
+
+    const response = await server.inject(options)
+
+    expect(response.statusCode).toBe(501)
+  })
+  /**
+   * summary: Quotes
+   * description: The HTTP request GET /bulkQuotes/{ID} is used to request the retrieval of a quote for the provided financial transaction in the server.
+   * parameters: Accept, Content-Length, Content-Type, Date, X-Forwarded-For, FSPIOP-Source, FSPIOP-Destination, FSPIOP-Encryption, FSPIOP-Signature, FSPIOP-URI, FSPIOP-HTTP-Method
+   * produces: application/json
+   * responses: 202, 400, 401, 403, 404, 405, 406, 501, 503
+   */
+  test('test Bulk Quotes by ID get operation', async () => {
+    const server = new Hapi.Server()
+
+    await server.register([{
+      plugin: HapiOpenAPI,
+      options: {
+        api: Path.resolve(__dirname, '../../src/interface/swagger.json'),
+        handlers: Path.join(__dirname, '../../src/handlers'),
+        outputvalidation: true
+      }
+    }, ErrorHandler])
+
+    const requests = new Promise((resolve, reject) => {
+      Mockgen().requests({
+        path: '/bulkQuotes/{ID}',
+        operation: 'get'
+      }, function (error, mock) {
+        return error ? reject(error) : resolve(mock)
+      })
+    })
+
+    const mock = await requests
+
+    expect(mock).toBeTruthy()
+    expect(mock.request).toBeTruthy()
+    // Get the resolved path from mock request
+    // Mock request Path templates({}) are resolved using path parameters
+    const options = {
+      method: 'get',
+      url: mock.request.path,
+      headers: helper.defaultHeaders()
+    }
+
+    const response = await server.inject(options)
+
+    expect(response.statusCode).toBe(501)
+  })
+  /**
+   * summary: Quotes
+   * description: The HTTP request PUT /bulkQuotes{ID} is used to request the creation of a quote for the provided financial transaction in the server.
+   * parameters: body, Accept, Content-Length, Content-Type, Date, X-Forwarded-For, FSPIOP-Source, FSPIOP-Destination, FSPIOP-Encryption, FSPIOP-Signature, FSPIOP-URI, FSPIOP-HTTP-Method
+   * produces: application/json
+   * responses: 202, 400, 401, 403, 404, 405, 406, 501, 503
+   */
+  test('test Bulk Quotes by ID put operation', async () => {
+    const server = new Hapi.Server()
+
+    await server.register([{
+      plugin: HapiOpenAPI,
+      options: {
+        api: Path.resolve(__dirname, '../../src/interface/swagger.json'),
+        handlers: Path.join(__dirname, '../../src/handlers'),
+        outputvalidation: true
+      }
+    }, ErrorHandler])
+
+    const requests = new Promise((resolve, reject) => {
+      Mockgen().requests({
+        path: '/bulkQuotes/{ID}',
+        operation: 'put'
+      }, function (error, mock) {
+        return error ? reject(error) : resolve(mock)
+      })
+    })
+
+    const mock = await requests
+
+    expect(mock).toBeTruthy()
+    expect(mock.request).toBeTruthy()
+    // Get the resolved path from mock request
+    // Mock request Path templates({}) are resolved using path parameters
+    const options = {
+      method: 'put',
+      url: mock.request.path,
+      headers: helper.defaultHeaders()
+    }
+    if (mock.request.body) {
+      // Send the request body
+      options.payload = mock.request.body
+    } else if (mock.request.formData) {
+      // Send the request form data
+      options.payload = mock.request.formData
+      // Set the Content-Type as application/x-www-form-urlencoded
+      options.headers = helper.defaultHeaders()
+    }
+
+    const response = await server.inject(options)
+
+    expect(response.statusCode).toBe(501)
+  })
+  /**
+   * summary: Quotes
+   * description: The HTTP request POST /bulkQuotes is used to request the creation of a quote for the provided financial transaction in the server.
+   * parameters: body, Accept, Content-Length, Content-Type, Date, X-Forwarded-For, FSPIOP-Source, FSPIOP-Destination, FSPIOP-Encryption, FSPIOP-Signature, FSPIOP-URI, FSPIOP-HTTP-Method
+   * produces: application/json
+   * responses: 202, 400, 401, 403, 404, 405, 406, 501, 503
+   */
+  test('test Bulk Quotes post operation', async () => {
+    const server = new Hapi.Server()
+
+    await server.register([{
+      plugin: HapiOpenAPI,
+      options: {
+        api: Path.resolve(__dirname, '../../src/interface/swagger.json'),
+        handlers: Path.join(__dirname, '../../src/handlers'),
+        outputvalidation: true
+      }
+    }, ErrorHandler])
+
+    const requests = new Promise((resolve, reject) => {
+      Mockgen().requests({
+        path: '/bulkQuotes',
+        operation: 'post'
+      }, function (error, mock) {
+        return error ? reject(error) : resolve(mock)
+      })
+    })
+
+    const mock = await requests
+
+    expect(mock).toBeTruthy()
+    expect(mock.request).toBeTruthy()
+    // Get the resolved path from mock request
+    // Mock request Path templates({}) are resolved using path parameters
+    const options = {
+      method: 'post',
+      url: mock.request.path,
+      headers: helper.defaultHeaders()
+    }
+    if (mock.request.body) {
+      // Send the request body
+      options.payload = mock.request.body
+    } else if (mock.request.formData) {
+      // Send the request form data
+      options.payload = mock.request.formData
+      // Set the Content-Type as application/x-www-form-urlencoded
+      options.headers = helper.defaultHeaders()
+    }
+
+    const response = await server.inject(options)
+
+    expect(response.statusCode).toBe(501)
+  })
+  /**
+   * summary: Quotes
+   * description: The HTTP request GET /health is used to request the retrieval of a quote for the provided financial transaction in the server.
+   * parameters: Accept, Content-Length, Content-Type, Date, X-Forwarded-For, FSPIOP-Source, FSPIOP-Destination, FSPIOP-Encryption, FSPIOP-Signature, FSPIOP-URI, FSPIOP-HTTP-Method
+   * produces: application/json
+   * responses: 202, 400, 401, 403, 404, 405, 406, 501, 503
+   */
+  test('test health get operation', async () => {
+    const server = new Hapi.Server()
+
+    await server.register([{
+      plugin: HapiOpenAPI,
+      options: {
+        api: Path.resolve(__dirname, '../../src/interface/swagger.json'),
+        handlers: Path.join(__dirname, '../../src/handlers'),
+        outputvalidation: true
+      }
+    }, ErrorHandler])
+
+    const requests = new Promise((resolve, reject) => {
+      Mockgen().requests({
+        path: '/health',
+        operation: 'get'
+      }, function (error, mock) {
+        return error ? reject(error) : resolve(mock)
+      })
+    })
+
+    const mock = await requests
+
+    expect(mock).toBeTruthy()
+    expect(mock.request).toBeTruthy()
+    // Get the resolved path from mock request
+    // Mock request Path templates({}) are resolved using path parameters
+    const options = {
+      method: 'get',
+      url: mock.request.path,
+      headers: helper.defaultHeaders()
+    }
+
+    const response = await server.inject(options)
+
+    expect(response.statusCode).toBe(502)
   })
 })
