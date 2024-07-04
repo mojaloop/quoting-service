07a0b52c5e327a168f42c15b64debb5d10e60bd4
Steven Oderayi <oderayi@gmail.com>
2024-07-04 10:26:20 +0100

    test: update integration test

--- test/integration/postRequest.test.js
+++ test/integration/postRequest.test.js
@@ -221,8 +221,7 @@ describe('POST request tests --> ', () => {
       await wait(WAIT_TIMEOUT)
 
       response = await hubClient.getHistory()
-      expect(response.data.history.length).toBe(1)
-
+      
       const request = response.data.history[0]
       expect(request.url).toBe(`/${proxyId}/quotes`)
       expect(request.body).toEqual(payload)
