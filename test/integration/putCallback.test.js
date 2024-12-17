3de6582f65de8df37dee440eb5ae9b36908e7184
Juan Correa <gibaros@users.noreply.github.com>
2024-12-17 02:40:07 -0500

    chore: sync integration test with minor iso branch

--- test/integration/putCallback.test.js
+++ test/integration/putCallback.test.js
@@ -4,11 +4,20 @@ const Config = require('../../src/lib/config')
 const dto = require('../../src/lib/dto')
 const mocks = require('../mocks')
 const MockServerClient = require('./mockHttpServer/MockServerClient')
+const { wrapWithRetries } = require('../util/helper')
 
-const hubClient = new MockServerClient()
+const TEST_TIMEOUT = 20_000
+const WAIT_TIMEOUT = 3_000
 
 describe('PUT callback Tests --> ', () => {
+  jest.setTimeout(TEST_TIMEOUT)
+
   const { kafkaConfig } = new Config()
+  const hubClient = new MockServerClient()
+  const retryConf = {
+    remainingRetries: process?.env?.TEST_INT_RETRY_COUNT || 20,
+    timeout: process?.env?.TEST_INT_RETRY_DELAY || 1
+  }
 
   beforeEach(async () => {
     await hubClient.clearHistory()
@@ -18,21 +27,54 @@ describe('PUT callback Tests --> ', () => {
     await Producer.disconnect()
   })
 
+  const base64Encode = (data) => Buffer.from(data).toString('base64')
+  const wait = (ms) => new Promise(resolve => setTimeout(resolve, ms))
+
+  /**
+   * Publishes a test 'POST quote' message to the Kafka topic
+   */
+  const createQuote = async ({
+    from = 'pinkbank',
+    to = 'greenbank',
+    amount = { amount: '100', currency: 'USD' },
+    amountType = 'SEND'
+  } = {}) => {
+    const { kafkaConfig } = new Config()
+    const { topic, config } = kafkaConfig.PRODUCER.QUOTE.POST
+    const topicConfig = dto.topicConfigDto({ topicName: topic })
+    const payload = mocks.postQuotesPayloadDto({ from, to, amount, amountType })
+    const message = mocks.kafkaMessagePayloadPostDto({ from, to, id: payload.quoteId, payloadBase64: base64Encode(JSON.stringify(payload)) })
+    const isOk = await Producer.produceMessage(message, topicConfig, config)
+    expect(isOk).toBe(true)
+    return payload
+  }
+
+  const getResponseWithRetry = async () => {
+    return wrapWithRetries(() => hubClient.getHistory(),
+      retryConf.remainingRetries,
+      retryConf.timeout,
+      (result) => result.data.history.length > 0
+    )
+  }
+
   test('should handle the JWS signing when a switch error event is produced to the PUT topic', async () => {
+    // create test quote to prevent db (row reference) error on PUT request
+    const quoteCreated = await createQuote()
+    await wait(WAIT_TIMEOUT)
+    await hubClient.clearHistory()
+
     let response = await hubClient.getHistory()
     expect(response.data.history.length).toBe(0)
 
     const { topic, config } = kafkaConfig.PRODUCER.QUOTE.PUT
     const topicConfig = dto.topicConfigDto({ topicName: topic })
-    const message = mocks.kafkaMessagePayloadDto()
+    const message = mocks.kafkaMessagePayloadDto({ id: quoteCreated.quoteId, operationId: 'QuotesByIDAndError' })
 
     const isOk = await Producer.produceMessage(message, topicConfig, config)
     expect(isOk).toBe(true)
+    response = await getResponseWithRetry()
 
-    await new Promise(resolve => setTimeout(resolve, 3000))
-
-    response = await hubClient.getHistory()
-    expect(response.data.history.length).toBe(1)
+    expect(response.data.history.length).toBeGreaterThan(0)
     const { headers, url } = response.data.history[0]
     expect(headers['fspiop-signature']).toBeTruthy()
     expect(url).toBe(`/${message.to}/quotes/${message.id}/error`)
--- test/mocks.js
+++ test/mocks.js
@@ -1,16 +1,23 @@
-const Config = new (require('../src/lib/config'))()
-const CONTENT_TYPE = 'application/vnd.interoperability.quotes+json;version=1.0'
+const idGenerator = require('@mojaloop/central-services-shared').Util.id
+const Config = require('../src/lib/config')
 
 const config = new Config()
 
+const CONTENT_TYPE = 'application/vnd.interoperability.quotes+json;version={{API_VERSION}}'
+const contentTypeFn = ({ fspiopVersion = 1.0 }) => CONTENT_TYPE.replace('{{API_VERSION}}', fspiopVersion)
+
+const generateULID = idGenerator({ type: 'ulid' })
+
 const kafkaMessagePayloadDto = ({
+  action = 'put',
   from = config.hubName,
   to = 'greenbank',
   id = 'aaab9c4d-2aac-42ef-8aad-2e76f2fac95a',
   type = 'quote',
-  action = 'put',
   payloadBase64 = 'eyJlcnJvckluZm9ybWF0aW9uIjp7ImVycm9yQ29kZSI6IjUxMDAiLCJlcnJvckRlc2NyaXB0aW9uIjoiRXJyb3IgZGVzY3JpcHRpb24ifX0=',
-  createdAtMs = Date.now()
+  createdAtMs = Date.now(),
+  fspiopVersion = '1.0',
+  operationId = 'QuotesByID'
 } = {}) => Object.freeze({
   from,
   to,
@@ -19,8 +26,8 @@ const kafkaMessagePayloadDto = ({
   content: {
     requestId: `${createdAtMs}:4015872a9e16:28:lsunvmzh:10002`,
     headers: {
-      'content-type': CONTENT_TYPE,
-      accept: CONTENT_TYPE,
+      'content-type': contentTypeFn({ fspiopVersion }),
+      accept: contentTypeFn({ fspiopVersion }),
       date: new Date(createdAtMs).toUTCString(),
       'fspiop-source': from,
       'fspiop-destination': to,
@@ -30,10 +37,10 @@ const kafkaMessagePayloadDto = ({
       connection: 'keep-alive',
       'content-length': '102'
     },
-    payload: `data:${CONTENT_TYPE};base64,${payloadBase64}`,
+    payload: `data:${contentTypeFn({ fspiopVersion })};base64,${payloadBase64}`,
     uriParams: { id },
     spanContext: {
-      service: 'QuotesErrorByIDPut',
+      service: operationId,
       traceId: 'aabbc4ff1f62cecc899cf5d8d51f42b7',
       spanId: '3aa852c7fa9edfbc',
       sampled: 0,
@@ -68,6 +75,45 @@ const kafkaMessagePayloadDto = ({
   }
 })
 
+const kafkaMessagePayloadPostDto = (params = {}) => kafkaMessagePayloadDto({
+  ...params,
+  action: 'post',
+  operationId: 'Quotes'
+})
+
+const postQuotesPayloadDto = ({
+  from = 'payer',
+  to = 'payee',
+  quoteId = generateULID(),
+  transactionId = generateULID(),
+  amountType = 'SEND',
+  amount = { amount: '100', currency: 'USD' },
+  transactionType = transactionTypeDto(),
+  payer = { partyIdInfo: { partyIdType: 'MSISDN', partyIdentifier: '987654321', fspId: from } },
+  payee = { partyIdInfo: { partyIdType: 'MSISDN', partyIdentifier: '123456789', fspId: to } }
+} = {}) => ({
+  quoteId,
+  transactionId,
+  amountType,
+  amount,
+  transactionType,
+  payer,
+  payee
+})
+
+const transactionTypeDto = ({
+  scenario = 'DEPOSIT',
+  initiator = 'PAYER',
+  initiatorType = 'CONSUMER'
+} = {}) => Object.freeze({
+  scenario,
+  initiator,
+  initiatorType
+})
+
 module.exports = {
-  kafkaMessagePayloadDto
+  kafkaMessagePayloadDto,
+  postQuotesPayloadDto,
+  kafkaMessagePayloadPostDto,
+  transactionTypeDto
 }
