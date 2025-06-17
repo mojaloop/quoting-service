4f08326825ebd0daa70c937907abb1c16e316573
Kevin Leyow <kleyow@gmail.com>
2025-06-17 07:27:50 -0500

    Update .ncurc.js
    Co-authored-by: Sam <10507686+elnyry-sam-k@users.noreply.github.com>

--- .ncurc.js
+++ .ncurc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  // Add a TODO comment indicating the reason for each rejected dependency upgrade added to this list, and what should be done to resolve it (i.e. handle it through a story, etc).
+  // Add a to-do comment (in CAPS) indicating the reason for each rejected dependency upgrade added to this list, and what should be done to resolve it (i.e. handle it through a story, etc).
   reject: [
     "eslint",
     "jest" // jest 30 is released but libraries that work with jest have not been updated to support it yet.
