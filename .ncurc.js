1218314741a0ac3ad9256451fe6b156c05f144ba
geka.evk <geka.evk@gmail.com>
2026-03-09 17:15:25 +0000

    feat(iad-582): updated central-services-shared version

--- .ncurc.js
+++ .ncurc.js
@@ -3,6 +3,7 @@ module.exports = {
   reject: [
     "eslint",
     "jest", // jest 30 is released but libraries that work with jest have not been updated to support it yet.
+    "@mojaloop/central-services-shared", // need to use 18.36.0-iad-582.* for now
     "@mojaloop/central-services-stream" // need to use 11.10.0-iad-582.* for now
   ]
 }
