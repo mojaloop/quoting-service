f25ca52a032f52d8932507b130decdcb0ed4d306
Justin Stevenson Theodorus <justin.theodorus@gmail.com>
2026-07-02 12:44:42 +0800

    chore(deps): update dependencies and resolve audit-ci findings

--- .ncurc.js
+++ .ncurc.js
@@ -2,6 +2,7 @@ module.exports = {
   // Add a to-do comment (in CAPS) indicating the reason for each rejected dependency upgrade added to this list, and what should be done to resolve it (i.e. handle it through a story, etc).
   reject: [
     "eslint",
-    "jest" // jest 30 is released but libraries that work with jest have not been updated to support it yet.
+    "jest", // jest 30 is released but libraries that work with jest have not been updated to support it yet.
+    "commander" // TODO: commander 15 is a MAJOR bump (drops Node 18, breaking API changes) used by src/handlers/index.js.
   ]
 }
