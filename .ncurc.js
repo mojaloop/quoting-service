4bd6153665f4c7404362c49bc34267d1711569fb
Justin Stevenson Theodorus <justin.theodorus@gmail.com>
2026-07-16 11:53:56 +0800

    chore(deps): update dependencies; hold event-sdk (serialize-error ESM breaks jest)

--- .ncurc.js
+++ .ncurc.js
@@ -3,6 +3,7 @@ module.exports = {
   reject: [
     "eslint",
     "jest", // jest 30 is released but libraries that work with jest have not been updated to support it yet.
-    "commander" // TODO: commander 15 is a MAJOR bump (drops Node 18, breaking API changes) used by src/handlers/index.js.
+    "commander", // TODO: commander 15 is a MAJOR bump (drops Node 18, breaking API changes) used by src/handlers/index.js.
+    "@mojaloop/event-sdk" // TODO: 14.8.5 bumps its serialize-error dep 8.1.0 -> 13.0.1 (ESM-only), which jest (CommonJS, no babel transform) cannot load. Unpin once jest supports the ESM dep (transform config) or event-sdk ships a CJS-compatible serialize-error.
   ]
 }
